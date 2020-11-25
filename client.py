f37bf0af7b3a36073cc98e9780833f79e03b260d
romanpalenik <brzdatv@gmail.com>
2020-11-25 17:29:51 +0100

    Uz sa posiela obrazok ale server to nevie detekovat

--- client.py
+++ client.py
@@ -1,11 +1,54 @@
 import socket
+import os
 from struct import *
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ip_address = input('Zadaj ip adresu')
-port = int(input("Zadaj port"))
-# send to tam davas ip, port
 
-client_socket.bind((ip_address, port))
-data = b'ahoj'
-client_socket.sendto(data, (ip_address, port-1))
+# function to create socket and send first packet to inicializate com
+def client_init():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip_address = '192.168.1.15'
+    port = int(input("Zadaj port"))
+    # send to tam davas ip, port
+
+    # through network you can send only bytes, you have to convert to bytes
+    data = pack('!c', 'I'.encode('ascii'))
+    client_socket.sendto(data, (ip_address, port))
+    return client_socket, ip_address, port
+
+
+def send_to_server(type_of_packet, send_info):
+    if type_of_packet == 'P':
+        messege = input("Zadaj spravu")
+        # data = pack('c', type_of_packet.encode('ascii')) + messege.encode()
+        data = type_of_packet.encode('ascii') + messege.encode()
+        send_info[0].sendto(data, (send_info[1], send_info[2]))
+
+    elif type_of_packet == 'M':
+        fragment_size = 512
+        file = open('img.jpeg', 'rb')
+        print(os.path.getsize('img.jpeg'))
+        fragment_count = (os.path.getsize('img.jpeg') // 512) + 1  # add +1, becouse // round down, you need has more space
+        print(fragment_count)
+        start_of_fragment = 0
+        end_of_fragment = 512
+        # send first packet with information about file and whole sending
+        header = 'M'.encode() + pack('!h', fragment_count)
+        send_info[0].sendto(header, (send_info[1], send_info[2]))
+
+        for fragment_number in range(fragment_count):
+
+            header = 'M'.encode() + pack('!h', fragment_size) + pack('!h', fragment_number)
+            data = header + file.read(fragment_size)
+            send_info[0].sendto(data, (send_info[1], send_info[2]))
+            start_of_fragment += fragment_size
+            end_of_fragment += fragment_size
+
+
+
+
+
+
+send_info = client_init()
+
+type_of_packet = input("Zadaj co chces poslat: ")
+send_to_server(type_of_packet, send_info)
--- server.py
+++ server.py
@@ -1,21 +1,50 @@
 import socket
+from binascii import hexlify
+
+all_packets = []
+number_packet_in_com = 0
+
+
+def put_together(packet):
+    global all_packets
+    all_packets.append(packet)
+
+
+def analyze_packet(packet):
+    global number_packet_in_com
+    type_of_packet = packet[0:1].decode('ascii')
+    if type_of_packet == 'I':
+        print('Spojenie bolo inicializovane')
+    elif type_of_packet == 'P':
+        print('Je to porno')
+        print('oh Gott, schneller')
+        print(packet[1:])
+    elif type_of_packet == 'M':
+        # fist packet with information of file
+
+        if number_packet_in_com == 0:
+            all_packet_number = packet[1:10]
+            print(packet)
+            print(all_packet_number)
+            print(int.from_bytes(all_packet_number, byteorder='big'))
+        else:
+            put_together(packet)
+
+        number_packet_in_com += 1
 
 
 def server_listen(port, s_socket):
     while True:
         print("Server is listening")
 
-        message, addres = s_socket.recvfrom(1500)
-
-        print("Sprava je", message)
-
+        packet, address = s_socket.recvfrom(1500)
+        analyze_packet(packet)
 
 
-host = socket.gethostname()
 port = int(input("Zadaj server port: "))
 
 s_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s_socket.bind((host, port))
+s_socket.bind(('', port))
 
 print("Spojenie bolo nastavene")
 
