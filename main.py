d2bd89f66eccb43930a581189f0011c031a61881
romanpalenik <brzdatv@gmail.com>
2020-11-29 00:42:45 +0100

    Premenovanie na main, opravenie chyby, a pridanie stratenie packetu

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import socket
 import os
+from time import perf_counter
 from struct import *
 import threading
 import time
 import crcmod
+import copy
 
 all_packets = []
 number_packet_in_com = 0
 all_packet_number = 0
 keep_alive_time = 5
 packet_number_to_simulate_error = 0
+is_sending = False
 
 
 def put_together(packet):
@@ -40,10 +43,7 @@ def reconstruction_from_bytes():
 def control_crc(packet) -> int:
     crc32 = crcmod.mkCrcFun(0x1EDC6F411, rev=False, initCrc=0xFFFFFFFF, xorOut=0x00000000)
     crc_value = crc32(packet)
-    if crc_value == 0:
-        return 1
-    else:
-        return 0
+    return crc_value
 
 
 def analyze_packet(packet, s_socket):
@@ -51,31 +51,39 @@ def analyze_packet(packet, s_socket):
     type_of_packet = packet[0:1].decode('ascii')
 
     if type_of_packet == 'I':
+        s_socket.settimeout(15)
         print('Spojenie bolo inicializovane')
         init_packet = pack('!c', 'I'.encode('ascii'))
         s_socket.sendto(init_packet, ('192.168.1.15', 5000))
 
     elif type_of_packet == 'P':
+        s_socket.settimeout(15)
         print('Je to porno')
         print('oh Gott, schneller')
         print(packet[1:])
         return False
 
     elif type_of_packet == 'M':
+        s_socket.settimeout(15)
         # fist packet with information of file
-        packet_number_to_simulate_error += 1
         if number_packet_in_com == 0:
             all_packet_number = int.from_bytes(packet[3:5], byteorder='big')
         else:
             # control if packet is alright
-            if control_crc(packet) == 0:
+            if control_crc(packet) != 0:
+                print('chyba je v ', number_packet_in_com)
                 error_message = pack('!c', 'E'.encode('ascii'))
                 s_socket.sendto(error_message, ('192.168.1.15', 5000))
-                return
-            put_together(packet)
+                return True
+            else:
+                put_together(packet)
 
+        # this block run if all packets were sent
         if number_packet_in_com == all_packet_number:
             print("Prebehlo poslanie jedneho suboru", number_packet_in_com)
+
+            right_packet = pack('!c', 'O'.encode('ascii'))
+            s_socket.sendto(right_packet, ('192.168.1.15', 5000))
 
             right_packet = pack('!c', 'O'.encode('ascii'))
             s_socket.sendto(right_packet, ('192.168.1.15', 5000))
@@ -84,14 +92,16 @@ def analyze_packet(packet, s_socket):
             number_packet_in_com = 0
             return False
 
+        number_packet_in_com += 1
         # packet is right
-        # simulating error, that acknowledgment was not send
-        if packet_number_to_simulate_error == 3:
-            return
+
+        # simulating error, that acknowledgment was lost
+        if number_packet_in_com == 3:
+            print("neposielam ACK")
+            return True
+
         right_packet = pack('!c', 'O'.encode('ascii'))
         s_socket.sendto(right_packet, ('192.168.1.15', 5000))
-
-        number_packet_in_com += 1
 
     elif type_of_packet == 'K':
         s_socket.settimeout(15)
@@ -103,9 +113,18 @@ def server_listen(s_socket):
     login = True
     while login:
         try:
+            # if server is in middle sending process ignore keep alive, you need timeout for missing packets
             packet, address = s_socket.recvfrom(1500)
-            login = analyze_packet(packet, s_socket)
+            type_of_packet = packet[0:1].decode('ascii')
+            if is_sending == True and type_of_packet == 'K':
+                pass
+            else:
+                login = analyze_packet(packet, s_socket)
         except TimeoutError:
+            s_socket.settimeout(15)
+            if is_sending:
+                error_message = pack('!c', 'E'.encode('ascii'))
+                s_socket.sendto(error_message, ('192.168.1.15', 5000))
             print('Skoncil server')
             return
 
@@ -153,7 +172,6 @@ def send_to_server(type_of_packet, send_info, add_error):
 
         fragment_size = 512
         file = open('img.jpeg', 'rb')
-        print(os.path.getsize('img.jpeg'))
         fragment_count = (os.path.getsize(
             'img.jpeg') // fragment_size) + 1  # add +1, because // round down, you need has more space
         print(fragment_count)
@@ -162,34 +180,45 @@ def send_to_server(type_of_packet, send_info, add_error):
         header = 'M'.encode() + pack('!h', fragment_size) + pack('!h', fragment_count)
         send_info[0].sendto(header, (send_info[1], send_info[2]))
 
+        check_packet, address = send_info[0].recvfrom(1500)
+        type_of_packet = check_packet[0:1].decode('ascii')
+
+        if type_of_packet == 'O':
+            pass
+
         for fragment_number in range(fragment_count):
 
             header = 'M'.encode() + pack('!h', fragment_size) + pack('!h', fragment_number)
             data = header + file.read(fragment_size)
             data = create_crc(data)
 
-            data = bytearray(data)
-
             # add error
             if fragment_number == 3 and add_error == 1:
-                data[fragment_size] = data[fragment_size] + 1
-
-            send_info[0].sendto(data, (send_info[1], send_info[2]))
+                data = bytearray(data)
+                corrupted_data = copy.deepcopy(data)
+                corrupted_data[2] = data[2] + 1
+                send_info[0].sendto(corrupted_data, (send_info[1], send_info[2]))
+            else:
+                send_info[0].sendto(data, (send_info[1], send_info[2]))
 
             # waiting if packet is alright
             while True:
 
                 try:
+                    send_info[0].settimeout(5)
                     check_packet, address = send_info[0].recvfrom(1500)
-                except TimeoutError:
-                    print('Neprisiel acknolegment z {}. packetu'.format(fragment_number))
+
+                except socket.timeout:
+                    print('Neprisiel acknolegment z {}. packetu'.format(fragment_number + 2))
                     send_info[0].sendto(data, (send_info[1], send_info[2]))
                     continue
 
                 # right packet continue, wrong send again
+
                 type_of_packet = check_packet[0:1].decode('ascii')
                 if type_of_packet == 'O':
                     break
+
                 elif type_of_packet == 'E':
                     print("Chyba bola v {} packetu".format(fragment_number))
                     send_info[0].sendto(data, (send_info[1], send_info[2]))
@@ -207,10 +236,10 @@ def send_keepalive(send_info):
 ### main
 def main():
     role = int(input("Zadaj 1 pre server: \nZadaj 2 pre clienta: \n3.Ukoncit\n:"))
+    s_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     if role == 1:
         port = int(input("Zadaj server port: "))
-        s_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s_socket.settimeout(1000)
+        s_socket.settimeout(15)
         s_socket.bind(('', port))
         print("Server ide")
         server_listen(s_socket)
@@ -218,14 +247,14 @@ def main():
         while True:
             choice = int(input('Chces byt stale server?'))
             if choice == 1:
-                s_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                s_socket.settimeout(15)
-                s_socket.bind(('', port))
-                print("Server ide")
                 server_listen(s_socket)
 
             elif choice == 2:
                 send_info = (s_socket, '192.168.1.15', 5000)
+
+                keep_alive_thread = threading.Thread(target=send_keepalive, args=(send_info,))
+                keep_alive_thread.start()
+
                 type_of_packet = input("Zadaj co chces poslat: ")
                 add_error = int(input("Zadaj ci chces pridat chybu: "))
                 send_to_server(type_of_packet, send_info, add_error)
@@ -243,10 +272,8 @@ def main():
         while True:
             choice = int(input('Chces byt stale client?'))
             if choice == 1:
-                client_socket = send_info[0]
-                client_socket.settimeout(15)
                 print("Server ide")
-                server_listen(client_socket)
+                server_listen(send_info[0])
 
             elif choice == 2:
                 type_of_packet = input("Zadaj co chces poslat: ")
